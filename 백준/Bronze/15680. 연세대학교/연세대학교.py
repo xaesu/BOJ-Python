34ef4710c95268e7211279a62b7369920a8c01e0
xaesu <xaesu6@gmail.com>
2025-01-14 23:06:28 +0900

    [Bronze V] Title: 연세대학교, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.py
@@ -0,0 +1,12 @@
+def output(word):
+    print('{}'.format(word))
+    return
+
+name = 'YONSEI'
+slogan = 'Leading the Way to the Future'
+
+num = int(input())
+if num == 0:
+    output(name)
+elif num == 1:
+    output(slogan)
